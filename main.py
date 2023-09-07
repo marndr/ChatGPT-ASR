ec287f06f7e7e3bed86848d77b9d5e0dec241ac4
Maryam NADERI <maryam.naderi@idiap.ch>
2023-09-08 00:16:16 +0200

    Update the main.py

--- main.py
+++ main.py
@@ -4,29 +4,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Set your OpenAI API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def correct_asr_errors(asr_outputs):
-    corrected_outputs = []
+def check_asr_errors(asr_outputs):
+    asr_results = []
 
     for asr_output in asr_outputs:
-        user_prompt = f"Correct the ASR error in the following sentence: '{asr_output}'"
+        user_prompt = f"Is there any ASR error in the sentence: '{asr_output}'?"
 
-        # Use ChatGPT to correct ASR error
+        # Use ChatGPT to check for ASR errors
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",  # Use GPT-3.5 Turbo model
+            model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "You are a helpful assistant that corrects ASR errors."},
+                {"role": "system", "content": "You are a helpful assistant that checks for ASR errors."},
                 {"role": "user", "content": user_prompt},
             ]
         )
 
-        # Extract and store the corrected sentence
-        corrected_sentence = response['choices'][0]['message']['content']
-        corrected_outputs.append(corrected_sentence)
+        # Extract the model's response
+        response_text = response['choices'][0]['message']['content']
+        asr_results.append(response_text)
 
-    return corrected_outputs
+    return asr_results
 
 if __name__ == "__main__":
     asr_outputs = [
@@ -40,14 +39,23 @@ if __name__ == "__main__":
         "I'll be their in five minuets.",
         "I'm going to the gross restore.",
         "I won't to go two the beech.",
-        "The son is shining brightly.",
-]
+        "The son is shining brightly in the sky.",
+        "The son sets at the beech are always stunting.",
+        "I prefer tea over coffee.",
+        "I need to catch a train to London.",
+        "I'll meet you at the coffee shop.",
+        "He's a professional chef.",
+        "The weather is nice today.",
+        "Please pass me the salt.",
+        "I'll be there in five minutes.",
+        "I'm going to the grocery store.",
+    ]
 
-    corrected_results = correct_asr_errors(asr_outputs)
+    asr_results = check_asr_errors(asr_outputs)
 
-    for i, (original, corrected) in enumerate(zip(asr_outputs, corrected_results)):
-        print(f"ASR Input {i + 1}: '{original}'")
-        print(f"Corrected Output {i + 1}: '{corrected}'")
+    for i, (original, result) in enumerate(zip(asr_outputs, asr_results), 1):
+        print(f"ASR Input {i}: '{original}'")
+        print(f"ASR Result {i}: '{result}'")
         print("=" * 50)
 
 
