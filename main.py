32f0af08f95ec587e4f29c52f0e54d8595e6dcd6
Maryam NADERI <maryam.naderi@idiap.ch>
2023-09-13 21:09:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import openai
 from dotenv import load_dotenv
 import re
 import numpy as np
+import json
 
-# Load API key from environment variables
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -15,6 +15,8 @@ def levenshtein_distance_custom(s1, s2, insert_cost=1, delete_cost=1, substitute
         # s1: reference, s2: hypothesis,
         s1 = custom_split(s1.lower())
         s2 = custom_split(s2.lower())
+        # s1 = s1.lower().split()
+        # s2 = s2.lower().split()
         m, n = len(s1), len(s2)
         dp = [[0] * (n+1) for _ in range(m+1)]
 
@@ -36,17 +38,18 @@ def levenshtein_distance_custom(s1, s2, insert_cost=1, delete_cost=1, substitute
         except ZeroDivisionError:
             wer = dp[m][n]
         return wer
-        
-def ask_chatgpt(question):
-    # Use ChatGPT to correct ASR errors for a single sentence
-    return openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant that corrects ASR errors."},
-            {"role": "user", "content": question},
-        ]
-    )['choices'][0]['message']['content']
 
+
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+
+delimiter = "####"
 dataset = [
         ("I prefer see over coffee.", "I prefer tea over coffee."),
         ("I need to catch a drain to London.", "I need to catch a train to London."),
@@ -60,27 +63,35 @@ dataset = [
         ("The son is shining brightly in the sky.", "The sun is shining brightly in the sky."),
         ("The son sets at the beech are always stunting.", "The sun sets at the beach are always stunning.")
     ]
+
+for i, (prompt, reference) in enumerate(dataset, 1):
+
+    messages =  [  
+        {'role':'system', 'content': f"""You are a helpful assistant that corrects ASR errors. \
+    You will be provided with the ASR output text delimited with {delimiter} characters. \
+    Provide multiple suggestions where the errors in the ASR text is corrected. Provide your output in json format with the \
+    keys: corrected_ASR_output and probability. Probability shows the likelihood of each correction. \
+    Do not output any additional text that is not in JSON format. \
+    Do not write any explanatory text after outputting the requested JSON.
+        """},    
+        {'role':'user', 'content':f'{delimiter}I meet pizza{delimiter}'},   
+        {'role':'assistant', 'content':'[{"response": "I eat pizza", "probability": "0.99"}]'},   
+        {'role':'user', 'content':f'{delimiter}{prompt}{delimiter}'}
+    ]
+    corrected_ASR_output = get_completion_from_messages(messages)
+    corrected_ASR_output = json.loads(corrected_ASR_output)
+
+    list.sort(corrected_ASR_output, key=lambda x: x["probability"], reverse=True) 
     
-# prompt = "Please correct any ASR errors in the following sentence: {}.  Put your answer into html h1 tag."
-# prompt = "Please correct any ASR errors in the following sentence: {} Put the corrected asr output into an html <h1> tag." 
-prompt = "Kindly rectify any ASR inaccuracies within the following sentence: '{}' and place the corrected ASR output inside an HTML <h1> tag." # the best
-#prompt = "Correct any ASR errors detected in the sentence: '{}', and insert the revised ASR output into an HTML <h1> tag. If there are several valid corrections, present them individually within HTML <h1> tags, ordered by decreasing likelihood."
+    response = corrected_ASR_output[0]["response"]
 
-for i, (asr_output, reference) in enumerate(dataset, 1):
-
-    question = prompt.format(asr_output)
-    response = ask_chatgpt(question)
+    wer = levenshtein_distance_custom(response, reference) 
     
     print(f"Test {i}")
-    print(f"question: {question}")
-    print(f"raw response: {response}")
-    
-    corrected_asr_output = re.findall('<h1>(.*?)</h1>', response)[0]
-    wer = levenshtein_distance_custom(corrected_asr_output, reference)
-    
-    print(f"Corrected ASR Output:   '{corrected_asr_output}'")
-    print(f"Reference:              '{reference}'")
+    print(f"ASR output:            {prompt}")
+    # print(f"Suggested corrections: {json.dumps(corrected_ASR_output, indent=2)}")
+    print(f"corrected ASR output:  {response}")
+    print(f"Reference:             {reference}")
     print(f"WER {wer}")
     print("=" * 50)
 
-    
