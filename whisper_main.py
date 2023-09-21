bd19bf41e1967f8320bef66b9d00a7a61d296cd0
Maryam NADERI <maryam.naderi@idiap.ch>
2023-09-21 23:19:28 +0200

    update whisper_main.py

--- whisper_main.py
+++ whisper_main.py
@@ -3,7 +3,7 @@ import re
 import openai
 import whisper_timestamped as whisper
 from dotenv import load_dotenv
-from jiwer import wer as jiwer_wer, cer as jiwer_cer, mer as jiwer_mer
+from jiwer import wer as jiwer_wer, cer as jiwer_cer
 import json
 
 delimiter = "####"
@@ -11,8 +11,7 @@ delimiter = "####"
 # Load API key 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
-
+  
 def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0.1):
     response = openai.ChatCompletion.create(
         model=model,
@@ -67,23 +66,32 @@ def transcribe(audio_filename):
     result = whisper.transcribe(model, audio, language="en")
     return result["text"]
 
+# Function to calculate SER
+def calculate_SER(ref, hyp):
+    pass
+    
 # Read audio filenames and transcriptions
 root = "/home/mnaderi/Documents/thesis/whisperii/LibriSpeech/dev-clean"
 librispeech_data = read_librispeech_transcriptions(root_folder=root)
 
+# Lists to store original and corrected ASR transcriptions
+original_asr_transcriptions = []
+corrected_asr_transcriptions = []
+
+f_out = open("results_whisper.md", "w")
 for i, (audio_path, reference_transcription) in enumerate(librispeech_data.items()):
 
     asr_transcription  = transcribe(audio_path)
     messages = [
         {'role': 'system', 'content': f"""You are a helpful assistant that corrects ASR errors. \
         You will be provided with the ASR output text delimited with {delimiter} characters. \
-        Provide multiple suggestions where the errors in the ASR text are corrected. Provide your output in json format with the \
-        keys: response and probability. response is the corrected_ASR_output and probability shows the likelihood of each correction. \
+        Provide multiple suggestions with the errors in the ASR text being corrected. Provide your output in json format with the \
+        keys: response and probability. response is the text with ASR errors being corrected and probability shows the likelihood of each correction. \
         Do not output any additional text that is not in JSON format. \
         Do not write any explanatory text after outputting the requested JSON.
             """},
         {'role': 'user', 'content': f'{delimiter}I meet pizza.{delimiter}'},
-        {'role': 'assistant', 'content': '[{"response": "I eat pizza.", "probability": "0.99"}]'},
+        {'role': 'assistant', 'content': '[{"response": "I eat pizza.", "probability": 0.99}]'},
         {'role': 'user', 'content': f'{delimiter}{asr_transcription}{delimiter}'}
     ]
 
@@ -100,36 +108,54 @@ for i, (audio_path, reference_transcription) in enumerate(librispeech_data.items
 
     corrected_asr_transcription = corrected_ASR_output[0]["response"]
 
-    # Calculate CER, SER, and WER using jiwer
-    corrected_asr_transcription = corrected_asr_transcription.lower()
-    reference_transcription = reference_transcription.lower()
-    
-    corrected_asr_transcription = remove_punctuations(corrected_asr_transcription )
-    reference_transcription = remove_punctuations(reference_transcription)
-    
-    CER = jiwer_cer(corrected_asr_transcription , reference_transcription) * 100
-    SER = jiwer_mer(corrected_asr_transcription , reference_transcription) * 100
-    WER = jiwer_wer(corrected_asr_transcription , reference_transcription) * 100
-
-
-    # calculate CER, SER, and WER using jiwer for original transcription and reference.
+    # Calculate CER and WER using jiwer for both original and corrected ASR transcriptions
     asr_transcription = asr_transcription.lower()
     asr_transcription = remove_punctuations(asr_transcription )
     
+    corrected_asr_transcription = corrected_asr_transcription.lower()
+    corrected_asr_transcription = remove_punctuations(corrected_asr_transcription )
+    
+    reference_transcription = reference_transcription.lower()
+    reference_transcription = remove_punctuations(reference_transcription)
+    
+    CER = jiwer_cer(corrected_asr_transcription , reference_transcription) * 100
+    
+    WER = jiwer_wer(corrected_asr_transcription , reference_transcription) * 100
+    
+    # SER = calculate_SER(corrected_asr_transcription , reference_transcription) * 100
+    
     CER_original = jiwer_cer(asr_transcription , reference_transcription) * 100
-    SER_original = jiwer_mer(asr_transcription , reference_transcription) * 100
+    
     WER_original = jiwer_wer(asr_transcription , reference_transcription) * 100
-     
-    print("=" * 50)
-    print(f"i: {i}")
-    print(f"ASR transcription:            {asr_transcription}")
+    
+    # SER_original = calculate_SER(asr_transcription , reference_transcription) * 100
+    
+    
+    print("---")
+    print(f"i: {i}\n")
+    print(f"ASR transcription:            {asr_transcription}\n")
     # print(f"Suggested corrections: {json.dumps(corrected_ASR_output, indent=2)}")
-    print(f"Corrected ASR transcription:  {corrected_asr_transcription}")
-    print(f"Reference transcription:             {reference_transcription}")
-    print(f"CER: {CER:.2f}%    CER(No chatgpt): {CER_original:.2f}%")
-    print(f"SER: {SER:.2f}%    SER(No chatgpt): {SER_original:.2f}%")
-    print(f"WER: {WER:.2f}%    WER(No chatgpt): {WER_original:.2f}%")
-    print("=" * 50)
+    print(f"Corrected ASR transcription:  {corrected_asr_transcription}\n")
+    print(f"Reference transcription:             {reference_transcription}\n")
+    print(f"CER: {CER:.2f}%    CER(No chatgpt): {CER_original:.2f}%\n")
+    #print(f"SER: {SER:.2f}%    SER(No chatgpt): {SER_original:.2f}%")
+    print(f"WER: {WER:.2f}%    WER(No chatgpt): {WER_original:.2f}%\n")
+    print("---")
 
-   
+    f_out.write(f"""
+    ## Test {i}
+    ASR transcription:            {asr_transcription}
+    Corrected ASR transcription:  {corrected_asr_transcription}
+    Reference transcription:      {reference_transcription}
+    CER: {CER:.2f}%    CER(No chatgpt): {CER_original:.2f}%%
+    WER: {WER:.2f}%    WER(No chatgpt): {WER_original:.2f}
+    ---
+    """)
+
+f_out.close()
+
+
+f_out.close()
+
+
 
