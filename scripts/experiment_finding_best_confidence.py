6d155a3b0ac474454d4cc29792e38ef86b1f9113
Maryam NADERI <maryam.naderi@idiap.ch>
2023-11-08 11:57:17 +0100

    Change the range of confidences based on histogram plot

--- scripts/experiment_finding_best_confidence.py
+++ scripts/experiment_finding_best_confidence.py
@@ -49,7 +49,7 @@ def evaluate_with_thresh(items, thresh):
     return wer, cer
 
 
-thresholds = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+thresholds = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9]
 results = []
 
 for thresh in thresholds:
