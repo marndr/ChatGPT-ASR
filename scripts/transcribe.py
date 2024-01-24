a8211e23cf05c89cb3e6de6139273a3010f89739
Enno Hermann <enno.hermann@idiap.ch>
2024-01-24 18:37:11 +0100

    style(scripts.transcribe): black auto-format

--- scripts/transcribe.py
+++ scripts/transcribe.py
@@ -1,5 +1,4 @@
 import json
-from pathlib import Path
 import argparse
 from dotenv import load_dotenv
 import os
@@ -9,40 +8,52 @@ from chat_gpt_asr.transcribe_whisper import transcribe
 from chat_gpt_asr.utils import read_librispeech_transcriptions
 
 if __name__ == "__main__":
-    
     parser = argparse.ArgumentParser(description="whisper model")
-    parser.add_argument("-wm", "--whisper_model", choices=["tiny","base","small", "medium", "large-v3"], default="tiny", help="Select the Whisper model")
-    parser.add_argument("-s", "--subset", choices=["dev-clean", "dev-other"], default="dev-clean", help="Librispeech subset to transcribe")
+    parser.add_argument(
+        "-wm",
+        "--whisper_model",
+        choices=["tiny", "base", "small", "medium", "large-v3"],
+        default="tiny",
+        help="Select the Whisper model",
+    )
+    parser.add_argument(
+        "-s",
+        "--subset",
+        choices=["dev-clean", "dev-other"],
+        default="dev-clean",
+        help="Librispeech subset to transcribe",
+    )
     args = parser.parse_args()
 
     load_dotenv()
     Root_Librispeech = os.getenv("ROOT_LIBRISPEECH")
     Root = os.getenv("ROOT_PATH")
-    
+
     output_folder = os.path.join(Root, "data/transcriptions")
-    output_file = os.path.join(output_folder, f"whisper_{args.whisper_model}_librispeech_{args.subset}-full.json")
+    output_file = os.path.join(
+        output_folder,
+        f"whisper_{args.whisper_model}_librispeech_{args.subset}-full.json",
+    )
 
-
-    data = read_librispeech_transcriptions(root_folder=os.path.join(Root_Librispeech,args.subset))
+    data = read_librispeech_transcriptions(
+        root_folder=os.path.join(Root_Librispeech, args.subset)
+    )
     l = []
     length = len(data)
-    
+
     for i, (audio_path, reference_transcription) in enumerate(data.items()):
-      try:
-        asr_transcription = transcribe(audio_path, args.whisper_model)
-        l.append(
-            {
-                "asr_transcription": asr_transcription,
-                "reference_transcription": reference_transcription,
-            }
-        )
-        print(f"{len(l)}/{length} completed!")
-      except:
-        continue 
-         
+        try:
+            asr_transcription = transcribe(audio_path, args.whisper_model)
+            l.append(
+                {
+                    "asr_transcription": asr_transcription,
+                    "reference_transcription": reference_transcription,
+                }
+            )
+            print(f"{len(l)}/{length} completed!")
+        except:
+            continue
+
     with open(output_file, "w") as f:
         json_str = json.dumps(l, indent=2)
         f.write(json_str)
-        
-        
-        
